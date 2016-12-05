a4742c38b40b3a8e5bc84bcb41dfc46e40adcc55
Adam Gapiński <gapinski.adam@outlook.com>
2016-12-05 10:36:57 +0100

    Created ArgumentParser.java Created ArgumentParser.java

---
+++ src/main/com/adam58/controller/ArgumentParser.java
@@ -0,0 +1,113 @@
+package com.adam58.controller;
+
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author Adam Gapiński
+ */
+public class ArgumentParser implements IArgumentParser {
+    @Override
+    public Path parseDocumentPath(String[] args) {
+        if (args.length == 0) {
+            throw new NotEnoughArgumentsException("Not file path specified.");
+        }
+
+        try {
+            return Paths.get(args[0]);
+        } catch (InvalidPathException e) {
+            throw new IllegalArgumentException("File path " + args[0] + " is invalid");
+        }
+    }
+
+    @Override
+    public UserRequest parseUserRequest(String[] args) {
+        if (args.length < 2) {
+            throw new NotEnoughArgumentsException("User request not specified");
+        }
+
+        String requestString = args[1];
+        requestString = requestString.substring(1, requestString.length());
+
+        UserRequest userRequest;
+        switch (requestString) {
+            case "ch":
+            case "c":
+                userRequest = UserRequest.Chapter;
+                break;
+            case "a":
+            userRequest = UserRequest.SingleArticle;
+                break;
+            case "ar":
+                userRequest = UserRequest.ArticleRange;
+                break;
+            default:
+                throw new IllegalArgumentException("Illegal user request");
+        }
+
+        return userRequest;
+    }
+
+    @Override
+    public int parseChapterNumber(String[] args) {
+        if (args.length < 3) {
+            throw new NotEnoughArgumentsException("Chapter number not specified");
+        }
+
+        return parseNumber(args[2], "chapter");
+    }
+
+    @Override
+    public int parseArticleNumber(String[] args) {
+        if (args.length < 3) {
+            throw new NotEnoughArgumentsException("Article number not specified");
+        }
+
+        return parseNumber(args[2], "article");
+    }
+
+    @Override
+    public ArticleRange parseArticleRange(String[] args) {
+        if (args.length < 3) {
+            throw new NotEnoughArgumentsException("Article range numbers are not specified");
+        }
+
+        String articleNumberString = args[2];
+        int firstArticleNumber;
+        int lastArticleNumber;
+
+        String[] stringRange = articleNumberString.split(" ");
+        if (stringRange.length == 1) {
+            stringRange = articleNumberString.split("-");
+
+            if (stringRange.length == 1) {
+                throw new NotEnoughArgumentsException("Article range not specified");
+            }
+        }
+
+        firstArticleNumber = parseNumber(stringRange[0].trim(), "article");
+        lastArticleNumber = parseNumber(stringRange[1].trim(), "article");
+
+        return new ArticleRange(firstArticleNumber, lastArticleNumber);
+    }
+
+    private int parseNumber(String stringNumber, String description) {
+        int number;
+
+        try {
+            number = Integer.valueOf(stringNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("%s is not valid %s number", stringNumber, description));
+        }
+
+        return number;
+    }
+
+    public static class NotEnoughArgumentsException extends RuntimeException {
+        public NotEnoughArgumentsException(String message) {
+            super(message);
+        }
+    }
+}
+
