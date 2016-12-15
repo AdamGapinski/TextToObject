ecfce3f8e043ecaf0541e9498baa11ab949e418a
Adam Gapiński <gapinski.adam@outlook.com>
2016-12-15 23:47:59 +0100

    Simplified Introduction.java class

--- src/main/com/adam58/model/Introduction.java
+++ src/main/com/adam58/model/Introduction.java
@@ -16,19 +16,4 @@ public class Introduction {
     public List<String> getContent() {
         return content;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Introduction that = (Introduction) o;
-
-        return content != null ? content.equals(that.content) : that.content == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return content != null ? content.hashCode() : 0;
-    }
 }
